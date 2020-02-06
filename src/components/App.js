bc3d2256cf20186665d5dddef08da574fe65353d
Tim <tk.tim.flanke@gmail.com>
2020-02-06 13:52:47 +0100

    add page not found component

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+// import {Button} from 'react-bootstrap';
 
-function App() {
+// import EventDetail from 'components/eventDetail/container';
+// import EventList from 'components/eventList/container';
+// import AddShow from 'components/addShow/container'
+// import Underground from 'components/underground/component';
+
+// import WikiListBands from 'components/wiki/wikiListBands/container';
+// import WikiBandDetail from 'components/wiki/wikiBandDetail/container';
+// import WikiListVenues from 'components/wiki/wikiListVenues/container';
+
+// import Header from 'components/header/component';
+// import Footer from 'components/footer/component';
+
+import PageNotFound from 'components/errors/component/pageNotFound'
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        {/* <Header /> */}
+          <div className="main-container">
+            <Switch>
+              {/* <Route exact path="/" render={() => (
+                <Redirect from="/" to="/EventList" />
+              )} />
+              <Route exact path="/EventList" component={EventList} />
+              <Route exact path="/EventDetail/:id" component={EventDetail} />
+              <Route exact path='/AddShow' component={AddShow} />
+              <Route exact path="/WikiBands" component={WikiListBands} />
+              <Route exact path="/WikiBands/:id" component={WikiBandDetail} />
+              <Route exact path="/WikiVenues" component={WikiListVenues} />
+
+              <Route exact path="/Underground" component={Underground} /> */}
+              <Route path='*' component={PageNotFound} />
+            </Switch>
+          </div>
+        {/* <Footer /> */}
+      </Router>
     </div>
   );
 }
