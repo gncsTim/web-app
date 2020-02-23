4d842d96016681b10f01f78a6a325886baf07b50
Tim <tk.tim.flanke@gmail.com>
2020-02-23 18:12:35 +0100

    update add event to couchdb over pouchdb

--- src/rdx/middlewares/couchdb/index.js
+++ src/rdx/middlewares/couchdb/index.js
@@ -4,15 +4,15 @@ import moment from 'moment'
 
 import history from 'rdx/history'
 import { remoteCouchdbUrl } from 'config'
-import { FETCH_EVENTS, ADD_EVENT, LOGIN_REQUEST, GET_SESSION, LOGOUT } from 'rdx/constants/actionTypes'
+import { ADD_EVENT, LOGIN_REQUEST, GET_SESSION, LOGOUT } from 'rdx/constants/actionTypes'
 import { setEventList, addOrUpdateEvents } from 'components/eventList/action'
 import { loginRequestFaild } from 'components/user/login/action'
 import { setUserCtx, resetUserCtx } from 'components/user/action'
 
 export const couchdbMiddleware = store => next => {
   PouchDB.plugin(PouchDBAuth)
-  console.log(remoteCouchdbUrl)
-  const remoteDB = new PouchDB(remoteCouchdbUrl, { skip_setup: true })
+  console.log(remoteCouchdbUrl('gncs'))
+  const remoteDB = new PouchDB(remoteCouchdbUrl('gncs'), { skip_setup: true })
   // remoteDB.info().then(function (result) {
   //   console.log(result)
   // }).catch(function (err) {
@@ -109,7 +109,41 @@ export const couchdbMiddleware = store => next => {
     })
     .catch(error => console.log(error))
 
-  const requests = new PouchDB('http://localhost:5984/requests')
+  const requests = new PouchDB('requests')
+  let syncRequests = PouchDB.replicate(requests, remoteCouchdbUrl('requests'), {
+    live: true,
+    retry: true,
+    filter: doc => {
+      console.log(doc)
+      if(!doc._deleted) return false
+      if(doc._id.match(/^_design\/.*/)) return true
+
+    }
+  })
+    .on('change', function(info) {
+      // handle change
+      console.log('handle change: ', info)
+    })
+    .on('paused', function(err) {
+      // replication paused (e.g. replication up to date, user went offline)
+      console.log('replication paused (e.g. replication up to date, user went offline): ', err)
+    })
+    .on('active', function() {
+      // replicate resumed (e.g. new changes replicating, user went back online)
+      console.log('replicate resumed (e.g. new changes replicating, user went back online)')
+    })
+    .on('denied', function(err) {
+      // a document failed to replicate (e.g. due to permissions)
+      console.log('a document failed to replicate (e.g. due to permissions): ', err)
+    })
+    .on('complete', function(info) {
+      // handle complete
+      console.log('handle complete: ', info)
+    })
+    .on('error', function(err) {
+      // handle error
+      console.log('handle error: ', err)
+    })
 
   return action => {
     const data = {
@@ -129,21 +163,59 @@ export const couchdbMiddleware = store => next => {
           })
         break
       case LOGIN_REQUEST:
-        remoteDB.logIn(action.payload.email, action.payload.password, function (err, response) {
+        remoteDB.logIn(action.payload.email, action.payload.password, function(err, response) {
           if (err) {
             store.dispatch(loginRequestFaild(err))
           } else {
             console.log(response)
-            store.dispatch(setUserCtx({
-              name: response.name,
-              roles: response.roles
-            }))
+            store.dispatch(
+              setUserCtx({
+                name: response.name,
+                roles: response.roles
+              })
+            )
+            if (response.roles.indexOf('_admin, editor')) {
+              syncRequests.cancel()
+              syncRequests = PouchDB.replicate(requests, remoteCouchdbUrl('gncs'), {
+                live: true,
+                retry: true
+              })
+                .on('change', function(info) {
+                  // handle change
+                  console.log('handle change: ', info)
+                })
+                .on('paused', function(err) {
+                  // replication paused (e.g. replication up to date, user went offline)
+                  console.log(
+                    'replication paused (e.g. replication up to date, user went offline): ',
+                    err
+                  )
+                })
+                .on('active', function() {
+                  // replicate resumed (e.g. new changes replicating, user went back online)
+                  console.log(
+                    'replicate resumed (e.g. new changes replicating, user went back online)'
+                  )
+                })
+                .on('denied', function(err) {
+                  // a document failed to replicate (e.g. due to permissions)
+                  console.log('a document failed to replicate (e.g. due to permissions): ', err)
+                })
+                .on('complete', function(info) {
+                  // handle complete
+                  console.log('handle complete: ', info)
+                })
+                .on('error', function(err) {
+                  // handle error
+                  console.log('handle error: ', err)
+                })
+            }
             history.push('/')
           }
         })
         break
       case GET_SESSION:
-        remoteDB.getSession(function (err, response) {
+        remoteDB.getSession(function(err, response) {
           if (err) {
             console.log(err)
           } else if (!response.userCtx.name) {
@@ -156,31 +228,80 @@ export const couchdbMiddleware = store => next => {
         })
         break
       case LOGOUT:
-        remoteDB.logOut(function (err, response) {
+        remoteDB.logOut(function(err, response) {
           if (err) {
             console.log(err)
           } else {
             console.log(response)
             store.dispatch(resetUserCtx())
+            syncRequests.cancel()
+            syncRequests = PouchDB.sync(remoteCouchdbUrl('requests'), requests, {
+              live: true,
+              retry: true
+            })
+              .on('change', function(info) {
+                // handle change
+                console.log('handle change: ', info)
+              })
+              .on('paused', function(err) {
+                // replication paused (e.g. replication up to date, user went offline)
+                console.log(
+                  'replication paused (e.g. replication up to date, user went offline): ',
+                  err
+                )
+              })
+              .on('active', function() {
+                // replicate resumed (e.g. new changes replicating, user went back online)
+                console.log(
+                  'replicate resumed (e.g. new changes replicating, user went back online)'
+                )
+              })
+              .on('denied', function(err) {
+                // a document failed to replicate (e.g. due to permissions)
+                console.log('a document failed to replicate (e.g. due to permissions): ', err)
+              })
+              .on('complete', function(info) {
+                // handle complete
+                console.log('handle complete: ', info)
+              })
+              .on('error', function(err) {
+                // handle error
+                console.log('handle error: ', err)
+              })
           }
         })
         break
       case ADD_EVENT:
+        // if (userCtx && userCtx.roles.indexOf('_admin', 'editor') !== -1) {
+        // console.log(userCtx.roles.indexOf('_admin', 'editor'))
+        // localDB.put(action.payload)
         requests
-          .put({
-            _id: 'mydoc',
-            type: 'request',
-            title: 'Heroes'
-          })
-          .then(function (response) {
+          .put(action.payload)
+          .then(response => {
             console.log(response)
-            // handle response
           })
-          .catch(function (err) {
-            console.log(err)
+          .catch(err => {
+            console.error(err)
           })
-        console.log(action.payload)
-        break
+        return console.log(action.payload)
+        // }
+        // TODO: add request show for user
+        // console.log('start request')
+        // requests
+        //   .put({
+        //     _id: 'mydoc',
+        //     type: 'request',
+        //     title: 'Heroes'
+        //   })
+        //   .then(function (response) {
+        //     console.log(response)
+        //     // handle response
+        //   })
+        //   .catch(function (err) {
+        //     console.log(err)
+        //   })
+        // console.log(action.payload)
+        // break
       default:
     }
     next(action)
@@ -206,12 +327,12 @@ const handleOnChange = store => change => {
 }
 
 const heanleOnPaused = store => info => {
-  console.log('replication paused.')
+  console.log('replication paused.', info)
   store.dispatch({ type: 'ADD_MSG', payload: 'replication paused.' })
 }
 
 const heanleOnActive = store => info => {
-  console.log('replication resumed.')
+  console.log('replication resumed.', info)
   store.dispatch({ type: 'ADD_MSG', payload: 'replication resumed.' })
 }
 
