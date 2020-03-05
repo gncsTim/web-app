1db4afe4482ad63045c24a7b45f98e75648637cb
kandziora <j.kandziora@gmail.com>
2020-03-05 15:00:44 +0100

    Update index.js

--- src/components/actors/request/reducer/index.js
+++ src/components/actors/request/reducer/index.js
@@ -2,7 +2,7 @@ import { SET_OWN_REQUEST, ADD_EVENT } from 'rdx/constants/actionTypes'
 
 export const ownRequestIdsReducer = (state = [], action) => {
   switch (action.type) {
-    case SET_OWNE_REQUEST:
+    case SET_OWN_REQUEST:
       return action.payload
     case ADD_EVENT:
       return state.concat(action.payload._id)
