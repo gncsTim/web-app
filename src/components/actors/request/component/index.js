af16d2cd5ab4a2dc1ee31c17f3756f7bb32bc2d8
kandziora <j.kandziora@gmail.com>
2020-03-05 15:02:09 +0100

    Update index.js

--- src/components/actors/request/component/index.js
+++ src/components/actors/request/component/index.js
@@ -6,10 +6,10 @@ import { userCtxShape }from 'gncsPropTypes'
 class RequestActor extends React.Component {
   componentDidUpdate(prevProps) {
     console.log('component did update prev props', prevProps)
-    const { owneRequestIds, setRequestSync, userCtx } = this.props
-    if (owneRequestIds.length > 0 && (owneRequestIds !== prevProps.owneRequestIds || userCtx !== prevProps.userCtx)) {
-      console.log('update own request prps: ', owneRequestIds)
-      setRequestSync(owneRequestIds)
+    const { ownRequestIds, setRequestSync, userCtx } = this.props
+    if (ownRequestIds.length > 0 && (ownRequestIds !== prevProps.ownRequestIds || userCtx !== prevProps.userCtx)) {
+      console.log('update own request prps: ', ownRequestIds)
+      setRequestSync(ownRequestIds)
     }
   }
   render() {
@@ -18,7 +18,7 @@ class RequestActor extends React.Component {
 }
 
 RequestActor.propTypes = {
-  owneRequestIds: PropTypes.arrayOf(PropTypes.string).isRequired,
+  ownRequestIds: PropTypes.arrayOf(PropTypes.string).isRequired,
   setRequestSync: PropTypes.func.isRequired,
   userCtx: PropTypes.shape(userCtxShape)
 }
