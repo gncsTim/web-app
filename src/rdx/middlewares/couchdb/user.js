26bba92b3686f8832f6de69f431cf87b5ae5549a
Tim <tk.tim.flanke@gmail.com>
2020-06-05 16:57:21 +0200

    work on proces user middleware

--- src/rdx/middlewares/couchdb/user.js
+++ src/rdx/middlewares/couchdb/user.js
@@ -1,6 +1,11 @@
 import history from 'rdx/history'
 import { remoteDB } from './index'
-import { LOGIN_REQUEST, GET_SESSION, LOGOUT } from 'rdx/constants/actionTypes'
+import {
+  LOGIN_REQUEST,
+  GET_SESSION,
+  LOGOUT,
+  CHANGE_PASSWORD_REQUEST
+} from 'rdx/constants/actionTypes'
 import { setUserCtx, resetUserCtx } from 'components/user/action'
 import { loginRequestFaild } from 'components/user/login/action'
 
@@ -40,6 +45,41 @@ export const userMiddleware = store => next => {
             console.log(err)
           } else {
             store.dispatch(resetUserCtx())
+          }
+        })
+        break
+      case CHANGE_PASSWORD_REQUEST:
+        console.log(store.getState().userCtx)
+        remoteDB.logIn(store.getState().userCtx.name, action.payload.oldPassword, function(
+          err,
+          response
+        ) {
+          if (err) {
+            console.log('change passwor faild')
+          } else {
+            console.log(response)
+            remoteDB.changePassword(
+              store.getState().userCtx.name,
+              action.payload.newPassword,
+              (err, response) => {
+                if (err) {
+                  console.error(err)
+                  if (err.name === 'not_found') {
+                    // typo, or you don't have the privileges to see this user
+                  } else {
+                    // some other error
+                  }
+                } else {
+                  console.log(response)
+                  // response is the user update response
+                  // {
+                  //   "ok": true,
+                  //   "id": "org.couchdb.user:spiderman",
+                  //   "rev": "2-09310a62dcc7eea42bf3d4f67e8ff8c4"
+                  // }
+                }
+              }
+            )
           }
         })
         break
